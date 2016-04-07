abefcf00e42eef995c12eaa45ec1ad97e64f34cd
kshkv <hlikosh@gmail.com>
2016-04-07 16:28:54 +0300

    dummy update

--- src/com/broccolibud/dummy/DummyEnvInfo.java
+++ src/com/broccolibud/dummy/DummyEnvInfo.java
@@ -1,5 +1,7 @@
 package com.broccolibud.dummy;
 
+import java.util.Random;
+
 import com.broccolibud.enums.DigitalDeviceState;
 import com.broccolibud.enums.SecurityCondition;
 import com.broccolibud.enums.SoilWaterState;
@@ -7,76 +9,107 @@ import com.broccolibud.enums.WaterTankState;
 import com.broccolibud.interfaces.EnvironmentInfoSource;
 
 public class DummyEnvInfo implements EnvironmentInfoSource {
-
+	
+	public static Random r = new Random();
+	public int tankState;
 	@Override
 	public double getTemperature() {
-		// TODO Auto-generated method stub
-		return 0;
+		//generating random double of temperature from 18°C to 40°C
+		double temperature = 0.0;
+		
+		double minT = 18.0;
+		double maxT = 40.0;
+		
+		temperature = minT + (maxT - minT) * r.nextDouble();
+		return temperature;// + "°C"
+		
 	}
 
 	@Override
 	public double getHumidity() {
-		// TODO Auto-generated method stub
-		return 0;
+		//generating random double of humidity from 0% to 100%
+		double humidity = 0.0;
+		
+		double minH = 0.0;
+		double maxH = 1.0;
+		
+		humidity = (minH + (maxH - minH) * r.nextDouble())*100;
+		return humidity;// + "%"
 	}
 
 	@Override
 	public DigitalDeviceState getMainLightState() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return null;//should be on || off
 	}
 
 	@Override
 	public DigitalDeviceState getSideLightState() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return null;//should be on || off
 	}
 
 	@Override
 	public WaterTankState getWaterTankState() {
-		// TODO Auto-generated method stub
-		return null;
+		int tank = 100;
+		int tankCondition = 0;
+		
+		int full = 1;
+		int poor = 2;
+		int empty = 3;
+		
+		tankCondition = tank - r.nextInt(100);
+		
+		if(tankCondition == tank){tankState = full;}
+		else if(tankCondition == (tank/2)){tankState = poor;}
+		else{tankState = empty;}
+		return tankState;//should be full || poor || empty
 	}
 
 	@Override
 	public SoilWaterState getSoilWaterState() {
-		// TODO Auto-generated method stub
-		return null;
+		int tank = 100;
+		int tankCondition = 0;
+		
+		tankCondition = tank - r.nextInt(100);
+		
+		
+		return null;//should be full || poor || empty
 	}
 
 	@Override
 	public DigitalDeviceState getMainVentilationState() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return null;//should be on || off
 	}
 
 	@Override
 	public DigitalDeviceState getAdditionalVentilationState() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return null;//should be on || off
 	}
 
 	@Override
 	public SecurityCondition getSecurityConditions() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return null;//should be on || off
 	}
 
 	@Override
 	public DigitalDeviceState getSecuritySwitchState() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return null;//should be on || off
 	}
 
 	@Override
 	public double getVoltage() {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public double getCurrent() {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
