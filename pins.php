43618ee8c47dd0993a8d4223bd7a6f674b44f084
Andrii Liakh <liakhandrii@gmail.com>
2017-04-12 12:07:20 +0300

    pins now have names

---
+++ pins.php
@@ -0,0 +1,12 @@
+<?php
+
+	define('WARM_LED', 24);
+	define('COLD_LED', 27);
+	define('FITO_LED', 25);
+	define('FAN', 28);
+	define('PUMP', 32);
+
+?>
+
+
+
--- timings.php
+++ timings.php
@@ -1,13 +1,15 @@
 <?php
 
+include 'pins.php';
+
 date_default_timezone_set("Europe/Kiev");
 
 # pin_number => [on time, off time]
      $timings = array(
-     	24 => array(12, 15),
-     	27 => array(15, 18),
-     	25 => array(4, 22),
-     	28 => array(4, 22)
+     	WARM_LED => array(12, 15),
+     	COLD_LED => array(15, 18),
+     	FITO_LED => array(4, 22),
+     	FAN => array(4, 22)
      );
 ?>
 
