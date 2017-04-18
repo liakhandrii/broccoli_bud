f56cfc714c5d158cc2dcfd7fb4f6afe00bad2bab
Andrii Liakh <liakhandrii@gmail.com>
2017-04-18 13:24:04 +0300

    I hate php

--- grow_settings.php
+++ grow_settings.php
@@ -1,29 +1,29 @@
 <?php
 
-	define(FLOWERING, 420);
-	define(VEGETATION, 1620);
+	define("FLOWERING", 420);
+	define("VEGETATION", 1620);
 	
-	// define(CYCLE, VEGETATION);
-	define(CYCLE, FLOWERING);
+	// define("CYCLE", VEGETATION);
+	define("CYCLE", FLOWERING);
 
-	define(WARM_LED, 24);
-	define(COLD_LED, 27);
-	define(FITO_LED, 25);
-	define(FAN, 28);
-	define(PUMP, 29);
+	define("WARM_LED", 24);
+	define("COLD_LED", 27);
+	define("FITO_LED", 25);
+	define("FAN", 28);
+	define("PUMP", 29);
 	
 	# twice a day
-	define(WATERINGS_PER_DAY, 2);
-	define(PUMP_TIMEOUT, ceil(60 * 60 * 24 / WATERINGS_PER_DAY));
+	define("WATERINGS_PER_DAY", 2);
+	define("PUMP_TIMEOUT", ceil(60 * 60 * 24 / WATERINGS_PER_DAY));
 	
 	# volume in mililiters
-	define(WATER_AMOUNT, 300)
-	define(PUMPING_TIME, ceil(300 / 14))
+	define("WATER_AMOUNT", 300)
+	define("PUMPING_TIME", ceil(300 / 14))
 	
-	define(MODE_AUTO, 228);
-	define(MODE_MANUAL, 322);
+	define("MODE_AUTO", 228);
+	define("MODE_MANUAL", 322);
 	
-	define(MODE, MODE_AUTO);
+	define("MODE", MODE_AUTO);
 
 ?>
 
