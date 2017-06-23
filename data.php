2ad7b62dfad09c6971840601f89dbe415c60e219
Andrii Liakh <liakhandrii@gmail.com>
2017-06-23 12:13:23 +0300

    inside light

--- data.php
+++ data.php
@@ -153,7 +153,7 @@ mysqli_close($conn);
 				if (mysqli_num_rows($result) > 0) {
 				    // output data of each row
 				    while($row = mysqli_fetch_assoc($result)) {
-				        echo $row["roomtemp"];
+				        echo $row["light"];
 				    }
 				} else {
 				    echo "0 results";
