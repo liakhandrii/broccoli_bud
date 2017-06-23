516e472a24d929db6cae7c4132af7696e8718210
Andrii Liakh <liakhandrii@gmail.com>
2017-06-23 13:05:59 +0300

    temperature based on/off

--- switch.php
+++ switch.php
@@ -31,38 +31,38 @@
 
 			}
 
-			$grow_temp = getGrowTemp();
-			echo($grow_temp);
-
-			if (SYSTEM_ON_TIME == -1 || SYSTEM_OFF_TIME == -1) {
-
-			} elseif (SYSTEM_ON_TIME < $off_time) {
-				if ($current_hour >= SYSTEM_ON_TIME && $current_hour < SYSTEM_OFF_TIME) {
-					if ($grow_temp > MAX_TEMP) {
-							system ( "gpio write ".WARM_LED." 1" );
-							system ( "gpio write ".COLD_LED." 1" );
-							system ( "gpio write ".FITO_LED." 0" );
-					} else if ($grow_temp < MIN_TEMP) {
-						system ( "gpio write ".WARM_LED." 0" );
-						system ( "gpio write ".COLD_LED." 0" );
-						system ( "gpio write ".FITO_LED." 0" );
-						system ( "gpio write ".FAN." 1" );
-					}
-				}
-			} else {
-				if ($current_hour >= SYSTEM_ON_TIME || $current_hour < SYSTEM_OFF_TIME) {
-					if ($grow_temp > MAX_TEMP) {
-							system ( "gpio write ".WARM_LED." 1" );
-							system ( "gpio write ".COLD_LED." 1" );
-							system ( "gpio write ".FITO_LED." 0" );
-					} else if ($grow_temp < MIN_TEMP) {
-						system ( "gpio write ".WARM_LED." 0" );
-						system ( "gpio write ".COLD_LED." 0" );
-						system ( "gpio write ".FITO_LED." 0" );
-						system ( "gpio write ".FAN." 1" );
-					}
-				}
-			}
+			// $grow_temp = getGrowTemp();
+			// echo($grow_temp);
+			//
+			// if (SYSTEM_ON_TIME == -1 || SYSTEM_OFF_TIME == -1) {
+			//
+			// } elseif (SYSTEM_ON_TIME < $off_time) {
+			// 	if ($current_hour >= SYSTEM_ON_TIME && $current_hour < SYSTEM_OFF_TIME) {
+			// 		if ($grow_temp > MAX_TEMP) {
+			// 				system ( "gpio write ".WARM_LED." 1" );
+			// 				system ( "gpio write ".COLD_LED." 1" );
+			// 				system ( "gpio write ".FITO_LED." 0" );
+			// 		} else if ($grow_temp < MIN_TEMP) {
+			// 			system ( "gpio write ".WARM_LED." 0" );
+			// 			system ( "gpio write ".COLD_LED." 0" );
+			// 			system ( "gpio write ".FITO_LED." 0" );
+			// 			system ( "gpio write ".FAN." 1" );
+			// 		}
+			// 	}
+			// } else {
+			// 	if ($current_hour >= SYSTEM_ON_TIME || $current_hour < SYSTEM_OFF_TIME) {
+			// 		if ($grow_temp > MAX_TEMP) {
+			// 				system ( "gpio write ".WARM_LED." 1" );
+			// 				system ( "gpio write ".COLD_LED." 1" );
+			// 				system ( "gpio write ".FITO_LED." 0" );
+			// 		} else if ($grow_temp < MIN_TEMP) {
+			// 			system ( "gpio write ".WARM_LED." 0" );
+			// 			system ( "gpio write ".COLD_LED." 0" );
+			// 			system ( "gpio write ".FITO_LED." 0" );
+			// 			system ( "gpio write ".FAN." 1" );
+			// 		}
+			// 	}
+			// }
 	}
 
 
